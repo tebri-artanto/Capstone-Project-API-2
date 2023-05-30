4fc6d45099a8192a9ebaa040af5e7824014f11a3
ArdiSasongko <ardisasongko002@gmail.com>
2023-05-30 16:45:27 +0700

    update

--- modules/Controller/pengepulController.js
+++ modules/Controller/pengepulController.js
@@ -17,7 +17,7 @@ const pengepulSignUp = async (req, res) => {
     const newPengepul = new Pengepul(request);
     const result = await newPengepul.save();
 
-    response = new Response.Success(false, "Success Adding Pengepul", result);
+    response = new Response.Success(false, null, result);
     res.status(httpStatus.OK).json(response);
   } catch (error) {
     response = new Response.Error(true, error.message);
@@ -42,7 +42,12 @@ const getPengepul = async (req, res) => {
 
     const pengepul = await Pengepul.find(query);
 
-    response = new Response.Success(false, null, pengepul);
+    if (pengepul.length === 0) {
+      response = new Response.Error(true, "No results found");
+    } else {
+      response = new Response.Success(false, "result found", pengepul);
+    }
+
     res.status(httpStatus.OK).json(response);
   } catch (error) {
     response = new Response.Error(true, error.message);
