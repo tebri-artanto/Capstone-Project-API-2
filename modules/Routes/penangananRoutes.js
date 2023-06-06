1aefe6477ce96dc5b33573b339ac9c72b86b2687
tebri-artanto <tebriartanto@gmail.com>
2023-06-06 17:36:35 +0700

    masih belum bisa predict

---
+++ modules/Routes/penangananRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = new express.Router();
+const penanganan = require("../Controller/penangananController");
+
+router.get("/", penanganan.getPenanganan)
+router.post("/add", penanganan.postPenanganan);
+
+module.exports = router;
---
+++ modules/Routes/predictRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = new express.Router();
+const predictController = require("../Controller/predictController");
+const requireAuth = require("../middleware/requireAuth");
+
+
+// router.use(requireAuth);
+router.post("/", predictController.upload, predictController.postPredictImage);
+// router.get("/", predictController.getPredict)
+// router.delete("/:id", predictController.deletePredict);
+// router.put("/:id", predictController.updatePredict);
+
+module.exports = router;
