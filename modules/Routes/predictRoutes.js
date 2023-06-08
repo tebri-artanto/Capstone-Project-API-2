bc3e63ec8100f938ea0e005c3eadde25c33ec959
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2023-06-08 12:08:46 +0700

    Update predictRoutes.js

--- modules/Routes/predictRoutes.js
+++ modules/Routes/predictRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = new express.Router();
 const predictController = require("../Controller/predictController");
-//const requireAuth = require("../middleware/requireAuth");
+const requireAuth = require("../middleware/requireAuth");
 
 
-// router.use(requireAuth);
+router.use(requireAuth);
 router.post("/", predictController.upload, predictController.postPredictImage);
 module.exports = router;
