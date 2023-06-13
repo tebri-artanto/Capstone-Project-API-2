bedf0e3d9c74b3217ba2ff19403c54da4922dc86
tebri-artanto <tebriartanto@gmail.com>
2023-06-13 23:02:02 +0700

    add secret manager

--- modules/Database/mongodb.js
+++ modules/Database/mongodb.js
@@ -1,11 +1,42 @@
 const mongoose = require("mongoose");
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
-mongoose
-  .connect(process.env.MONGO_DB, {
+async function getSecretValue() {
+  const client = new SecretManagerServiceClient();
+  const name = 'projects/377381526885/secrets/database/versions/1';
+
+  const [version] = await client.accessSecretVersion({
+    name: name,
+  });
+
+  const connectionString = version.payload.data.toString();
+
+  mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Database Connected"))
-  .catch((error) => console.log(error.message));
+    .then(() => console.log("Database Connected"))
+    .catch((error) => console.log(error.message));
+}
+getSecretValue().catch(console.error);
+// async function getSecretValue() {
+//   const client = new SecretManagerServiceClient();
+//   const name = 'projects/377381526885/secrets/database/versions/1';
+
+//   const [version] = await client.accessSecretVersion({
+//     name: name,
+//   });
+
+//   const payload = version.payload.data.toString();
+
+//   console.log('Secret value:', payload);
+// }
+  
+// mongoose.connect("string", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Database Connected"))
+//   .catch((error) => console.log(error.message));
 
   
