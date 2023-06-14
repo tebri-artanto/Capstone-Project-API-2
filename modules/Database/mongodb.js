f020b3d02582ffec665c43ae8f79aa7815e00865
tebri-artanto <tebriartanto@gmail.com>
2023-06-14 21:17:40 +0700

    Update mongodb.js

--- modules/Database/mongodb.js
+++ modules/Database/mongodb.js
@@ -1,28 +1,39 @@
 const mongoose = require("mongoose");
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
-async function getSecretValue() {
-  const client = new SecretManagerServiceClient();
-  const name = 'projects/631861842917/secrets/database/versions/1';
+const client = new SecretManagerServiceClient();
+const getSecretValue = async () => {
 
   const [version] = await client.accessSecretVersion({
-    name: name,
+    name: 'projects/631861842917/secrets/database/versions/1',
   });
-
   const connectionString = version.payload.data.toString('utf8');
   return connectionString;
-
-  
 }
 
-const connectionString = getSecretValue();
+const connectDb = async () => {
+  try {
+    const connectionString = await getSecretValue();
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('Database Connected');
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log("Database Connected"))
-  .catch((error) => console.log(error.message));
+connectDb();
+
+// const connectionString = getSecretValue();
+
+// mongoose.connect(connectionString, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log("Database Connected"))
+//   .catch((error) => console.log(error.message));
 // async function getSecretValue() {
 //   const client = new SecretManagerServiceClient();
 //   const name = 'projects/377381526885/secrets/database/versions/1';
